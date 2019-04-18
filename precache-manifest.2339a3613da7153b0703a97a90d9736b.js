52d59ecbbb07be3e21fe0b177f4d0032e8ab82d6
Duha-H <duha.h.153@gmail.com>
2019-04-17 21:50:15 -0400

    Updates

---
+++ precache-manifest.2339a3613da7153b0703a97a90d9736b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GraphicDesign-Portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a8fca852c9fa8e722fa5",
+    "url": "/GraphicDesign-Portfolio/static/js/runtime~main.5644d7e9.js"
+  },
+  {
+    "revision": "7bdf1bc1923768446b86",
+    "url": "/GraphicDesign-Portfolio/static/js/main.cc925d3f.chunk.js"
+  },
+  {
+    "revision": "1795a45bb533afa1c9af",
+    "url": "/GraphicDesign-Portfolio/static/js/2.7c3ac910.chunk.js"
+  },
+  {
+    "revision": "7bdf1bc1923768446b86",
+    "url": "/GraphicDesign-Portfolio/static/css/main.9d9ba836.chunk.css"
+  },
+  {
+    "revision": "6d8b6e21d2686b5ea1c087ac01ca6a27",
+    "url": "/GraphicDesign-Portfolio/index.html"
+  }
+];
